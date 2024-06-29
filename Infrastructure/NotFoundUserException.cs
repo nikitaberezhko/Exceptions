4920ddfd992a87d88c8464292c202b0c0681fef8
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-06-29 16:06:08 +0300

    Initial commit

---
+++ Infrastructure/NotFoundUserException.cs
@@ -0,0 +1,6 @@
+namespace Exceptions.Services;
+
+public class NotFoundUserException : Exception
+{
+    
+}
---
+++ Services/ValidationException.cs
@@ -0,0 +1,6 @@
+namespace Exceptions.Services;
+
+public class ValidationException : Exception
+{
+    
+}
